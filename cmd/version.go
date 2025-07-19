e55b7a11fcf522c6bb11de36a38ba3a5f1a21676
Ivan Knoefler <68387388+Erl-koenig@users.noreply.github.com>
2025-07-19 19:35:16 +0200

    update short text versionCmd

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ var version = "dev"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Show the version of your CLI tool",
+	Short: "Show the version of switchdl",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("switchdl %s\n", version)
 	},
